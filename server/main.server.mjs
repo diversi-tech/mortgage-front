c69def0fa69eab47d63d4e215f6d3cb6c08d760e
mirimoshe <mirimoshe2004@gmail.com>
2024-07-11 18:04:39 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,11 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  AppServerModule
-} from "./chunk-ZQQ3VB6W.mjs";
-import "./chunk-6EJSOFRY.mjs";
-import "./chunk-3X2HKFG6.mjs";
-import "./chunk-TAYT5OZE.mjs";
-export {
-  AppServerModule as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-4GBSFYBW.mjs";import"./chunk-QLWW7T3T.mjs";import"./chunk-MDDGD3BJ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
