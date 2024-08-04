759a7870f051a60d900ae8be3186103c4c78963b
mirimoshe <mirimoshe2004@gmail.com>
2024-08-04 13:25:48 +0300

    lead route change and customer update change

--- mortgage-front/src/app/lead/lead-routing.module.ts
+++ mortgage-front/src/app/lead/lead-routing.module.ts
@@ -6,23 +6,23 @@ import { authGuardLead } from '../auth/auth.guard';
 import { PageNotFoundComponent } from '../global/page-not-found/page-not-found.component';
 //import { authGuardLead } from '../auth/auth.guard';
 
-// const routes: Routes = [
-//     { path: '', component: LeadComponent },
-//     { path: 'lead/:id', component: LeadComponent },
-//     { path: 'magic-link', component: MagicLinkComponent },
-// ];
-
-const routes: Routes = [{
-    path: '', component: LeadComponent,
-    canActivate: [authGuardLead],
-    children: [
-        //{ path: '', component: LeadComponent },
-        { path: 'lead/:id', component: LeadComponent },
-        { path: 'magic-link', component: MagicLinkComponent },
-    ]
-}
-
+const routes: Routes = [
+    { path: '', component: LeadComponent },
+    { path: 'lead/:id', component: LeadComponent },
+    { path: 'magic-link', component: MagicLinkComponent },
 ];
+
+// const routes: Routes = [{
+//     path: '', component: LeadComponent,
+//     canActivate: [authGuardLead],
+//     children: [
+//         //{ path: '', component: LeadComponent },
+//         { path: 'lead/:id', component: LeadComponent },
+//         { path: 'magic-link', component: MagicLinkComponent },
+//     ]
+// }
+
+//];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- mortgage-front/src/app/shared/Services/costumer.service.ts
+++ mortgage-front/src/app/shared/Services/costumer.service.ts
@@ -7,7 +7,7 @@ import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class customerService {
-  readonly basicURL = "https://localhost:7055/api/";
+  readonly basicURL = `${environment.apiURL}/api/`;
   private customersSubject = new BehaviorSubject<ICustomer[]>([]);
   customers$ = this.customersSubject.asObservable();
 
@@ -46,10 +46,10 @@ export class customerService {
   }
   getCustomerById(id: number): ICustomer | undefined {
     const customers = this.customersSubject.getValue();
-    const c=customers.find(customer => customer.id === id);
+    const c = customers.find(customer => customer.id === id);
     return c;
   }
-  getById(id: number): Observable<any>{
+  getById(id: number): Observable<any> {
     return this.http.get(`${this.basicURL}Customers/${id}`); // Use this.apiUrl
   }
 
@@ -67,8 +67,20 @@ export class customerService {
     return this.http.post<ICustomer>(`${this.basicURL}Customers`, customer);
   }
 
-  updateCustomer(customerId: number|undefined, customer: ICustomer|undefined): Observable<ICustomer> {
-    return this.http.put<ICustomer>(`${this.basicURL}Customers/${customerId}`,customer);
+  updateCustomer(customerId: number | undefined, customer: ICustomer): Observable<ICustomer> {
+    console.log('customer',customer);
+    
+    return this.http.put<ICustomer>(`${this.basicURL}Customers/${customerId}`, customer).pipe(
+      tap(() => {
+        const customers = this.customersSubject.getValue();
+        const index =customers.findIndex(c => c.id === customerId);
+        if (index !== -1) {
+          customers[index] = customer;        
+          this.customersSubject.next([...customers]);
+          this.fetchCustomers().subscribe(); 
+        }  
+      })
+    )
   }
 }
 
