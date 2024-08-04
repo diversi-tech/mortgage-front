1d2f3b580d32ccac3af138e6d211a2bb018fdf67
mirimoshe <mirimoshe2004@gmail.com>
2024-08-04 15:09:36 +0300

    Auto-generated commit

---
+++ browser/chunk-N4VE4RPT.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-E3LJAAMW.js";import"./chunk-2VDLTEOX.js";import"./chunk-RYVI6X3R.js";import"./chunk-72IXFK7V.js";export{a as AdminModule};
--- chunk-TGYYILDN.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-W6HRDZZN.js";import"./chunk-2VDLTEOX.js";import"./chunk-RYVI6X3R.js";import"./chunk-72IXFK7V.js";export{a as AdminModule};
