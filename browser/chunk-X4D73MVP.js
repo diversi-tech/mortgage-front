d9e79914e292b48b221e602f614bdfd10a43c1fe
mirimoshe <mirimoshe2004@gmail.com>
2024-08-04 22:21:03 +0300

    Auto-generated commit

---
+++ browser/chunk-X4D73MVP.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-6GGJ7ZHF.js";import"./chunk-F5QCNMIO.js";import"./chunk-2VDLTEOX.js";import"./chunk-RYVI6X3R.js";import"./chunk-72IXFK7V.js";export{a as LeadModule};
---
+++ server/chunk-RCMNZY4Z.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GUR6WN7U.mjs";import"./chunk-MOPF5JQC.mjs";import"./chunk-QQ6DYRED.mjs";import"./chunk-B3SZUYRB.mjs";import"./chunk-TIRFLIF6.mjs";import"./chunk-ZE4Y54UW.mjs";import"./chunk-NDYDZJSS.mjs";export{a as LeadModule};
