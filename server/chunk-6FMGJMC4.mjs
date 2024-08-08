3af90d80dd72f577369ba161578e0a4026c59196
mirimoshe <mirimoshe2004@gmail.com>
2024-08-08 14:50:32 +0300

    Auto-generated commit

---
+++ server/chunk-6FMGJMC4.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{c as a}from"./chunk-V75PG2S7.mjs";import"./chunk-OQZJJH7Y.mjs";import"./chunk-NW2DFKJ6.mjs";import"./chunk-MR55LIO3.mjs";import"./chunk-YZM5RFRX.mjs";import"./chunk-NDYDZJSS.mjs";export{a as AdminModule};
