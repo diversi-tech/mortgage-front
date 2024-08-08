b71d5a77db8f74895155f04ffc477aa52b94dafe
mirimoshe <mirimoshe2004@gmail.com>
2024-08-08 14:59:50 +0300

    Auto-generated commit

--- chunk-FWY3TTRD.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-G42VYA4M.js";import"./chunk-PCRZE4HB.js";import"./chunk-GZYZNWGU.js";import"./chunk-IRQTURN7.js";export{a as AdminModule};
---
+++ chunk-RGBMEK54.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-HNEVYLRP.js";import"./chunk-PCRZE4HB.js";import"./chunk-GZYZNWGU.js";import"./chunk-IRQTURN7.js";export{a as AdminModule};
