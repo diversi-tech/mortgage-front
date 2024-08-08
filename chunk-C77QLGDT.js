687da707aebaae20a2767724c6df9f612d80277f
mirimoshe <mirimoshe2004@gmail.com>
2024-08-08 15:47:35 +0300

    Auto-generated commit

---
+++ chunk-C77QLGDT.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-OB6JK7DN.js";import"./chunk-FOCA34K7.js";import"./chunk-GZYZNWGU.js";import"./chunk-IRQTURN7.js";export{a as AdminModule};
--- chunk-W5DO2FLM.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-HDOP4IVS.js";import"./chunk-FOCA34K7.js";import"./chunk-GZYZNWGU.js";import"./chunk-IRQTURN7.js";export{a as AdminModule};
