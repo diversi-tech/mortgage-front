8b322885c6fb55cf344d129b9f986a5180380fcc
mirimoshe <mirimoshe2004@gmail.com>
2024-09-24 15:49:31 +0300

    Auto-generated commit

---
+++ chunk-7EG2LAAK.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OW3U6K43.js";import"./chunk-42AVWSWG.js";import"./chunk-4TWN7O3O.js";import"./chunk-55DLLJ4O.js";import"./chunk-VEZ2VVRJ.js";import"./chunk-W3PUFPAQ.js";export{a as LeadModule};
