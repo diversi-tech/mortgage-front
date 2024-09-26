43fa92857eda76debd81df522732f12c36c6f41a
mirimoshe <mirimoshe2004@gmail.com>
2024-09-26 13:13:19 +0300

    Auto-generated commit

--- chunk-VTJGV4A6.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-33W5NMSL.js";import"./chunk-GSI6USGQ.js";import"./chunk-4TGRP7LC.js";import"./chunk-DRVQ54QN.js";import"./chunk-VEZ2VVRJ.js";import"./chunk-W3PUFPAQ.js";export{a as LeadModule};
