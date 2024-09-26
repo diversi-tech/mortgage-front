a6d656b34cff794483950ef62f325a2a890211a5
mirimoshe <mirimoshe2004@gmail.com>
2024-09-26 13:37:18 +0300

    Auto-generated commit

---
+++ chunk-UXIXAITT.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-S2OKDIOB.js";import"./chunk-GSI6USGQ.js";import"./chunk-4TGRP7LC.js";import"./chunk-DRVQ54QN.js";import"./chunk-VEZ2VVRJ.js";import"./chunk-W3PUFPAQ.js";export{a as LeadModule};
